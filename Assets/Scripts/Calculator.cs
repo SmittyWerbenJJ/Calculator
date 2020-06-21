09a0feaf0765827c94b6cd89349865e0b8d24aba
Smitty Werben <smittywerbenjj.1@gmail.com>
2020-06-21 22:34:49 +0200

    Added Previous Project Files

---
+++ Assets/Scripts/Calculator.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Calculator
+{
+    public enum eNumpad_buttons
+    {
+        Numbers,
+        Percent,
+        Invert,
+        Brackets,
+        plus,
+        minus,
+        multiply,
+        divide,
+        DeleteAll,
+        DeleteLast,
+        Comma,
+        Submit
+
+    }
+
+
+    public class Calculator
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
