f2295cde686c87db88b5343c2cfc74912c23172c
Smitty Werben <smittywerbenjj.1@gmail.com>
2020-06-21 23:15:10 +0200

    Added Assets

---
+++ Scripts/Calc_Numpad.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Calc_Numpad : MonoBehaviour
+{
+    public Calc_Button[] keys;
+
+
+    void Start()
+    {
+        keys = GetComponentsInChildren<Calc_Button>();
+
+    }
+
+
+}
